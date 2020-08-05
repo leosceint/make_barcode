c425c84ffbe7b0cd71ca5a40bba18fd22c6aed05
Lev Grodzickiy <piyaotoi@gmail.com>
2020-08-05 17:29:01 +0300

    Без переворачивания и без учета границ в разрядности

--- Bitset.cpp
+++ Bitset.cpp
@@ -8,21 +8,23 @@ vBitset(size, 0)
 
 std::vector<bool> Bitset::Bits(int number)
 {
-    int index = 0;
+    int index = vBitset.size() - 1;
     while(number)
     {
-        vBitset[index++] = number%2;
+        if (index < 0)
+            break;
+        vBitset[index--] = number%2;
         number >>= 1;
     }
 
     //revert
-    int bitset_size = vBitset.size();
-    for(int i=0; i < bitset_size/2; i++)
-    {
-        bool t = vBitset[i];
-        vBitset[i] = vBitset[bitset_size-i-1];
-        vBitset[bitset_size-i-1] = t;
-    }
+    // int bitset_size = vBitset.size();
+    // for(int i=0; i < bitset_size/2; i++)
+    // {
+    //     bool t = vBitset[i];
+    //     vBitset[i] = vBitset[bitset_size-i-1];
+    //     vBitset[bitset_size-i-1] = t;
+    // }
 
     return vBitset;
 }
