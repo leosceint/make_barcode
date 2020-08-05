0dbc8f1aa1b1b00539eb77d36dc4879f4766e206
Lev Grodzickiy <piyaotoi@gmail.com>
2020-08-05 17:35:17 +0300

    Удалил лишние комментарии

--- Bitset.cpp
+++ Bitset.cpp
@@ -17,14 +17,5 @@ std::vector<bool> Bitset::Bits(int number)
         number >>= 1;
     }
 
-    //revert
-    // int bitset_size = vBitset.size();
-    // for(int i=0; i < bitset_size/2; i++)
-    // {
-    //     bool t = vBitset[i];
-    //     vBitset[i] = vBitset[bitset_size-i-1];
-    //     vBitset[bitset_size-i-1] = t;
-    // }
-
     return vBitset;
 }
